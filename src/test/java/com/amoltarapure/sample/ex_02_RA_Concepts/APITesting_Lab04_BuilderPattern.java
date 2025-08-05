c8ef3abd1a8a9495b0646e3af3f90b79bd859d5c
AmolTarapure16 <amoltarapure16@gmail.com>
2025-08-05 21:43:22 +0530

    5-Aug - com.amoltarapure.sample ex_01_RA_Basics ex_02_RA_Concepts ex_03_TestNG_AllureReoprt

---
+++ src/test/java/com/amoltarapure/sample/ex_02_RA_Concepts/APITesting_Lab04_BuilderPattern.java
@@ -0,0 +1,31 @@
+package com.amoltarapure.sample.ex_02_RA_Concepts;
+
+public class APITesting_Lab04_BuilderPattern
+{
+
+    public APITesting_Lab04_BuilderPattern step1()
+    {
+        System.out.println("Step-1");
+        return this;
+    }
+
+
+    public APITesting_Lab04_BuilderPattern step2()
+    {
+        System.out.println("Step-2");
+        return this;
+    }
+
+    public APITesting_Lab04_BuilderPattern step3(String S)
+    {
+        System.out.println("Step-3");
+        return this;
+    }
+
+    public static void main(String[] args) {
+        APITesting_Lab04_BuilderPattern builderPattern = new APITesting_Lab04_BuilderPattern();
+        builderPattern.step1().step2().step3("Amol");
+
+    }
+
+}
